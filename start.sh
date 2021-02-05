0bc320d568e4181b838e42a12dab9eddb577b107
Matej Pavlovic <mpa@zurich.ibm.com>
2021-02-05 17:56:02 +0100

    Augment start.sh to start a client as well
    Signed-off-by: Matej Pavlovic <mpa@zurich.ibm.com>

--- start.sh
+++ start.sh
@@ -2,19 +2,34 @@
 
 set -ex
 
+# Create binaries.
 go build ./cmd/bootstrap
 go build ./cmd/node
+go build ./cmd/client
 
-killall node &> /dev/null || true
+# Clean up.
+killall node client &> /dev/null || true
 rm -Rf bootstrap.d
 mkdir -p bootstrap.d
 
+# Generate new configuration.
 ./bootstrap
 
+# Start nodes.
 for ii in $(seq 0 3) ; do
 	CONFIG="bootstrap.d/node${ii}/config/node-config.yaml"
 	RUNDIR="bootstrap.d/node${ii}/run/"
 	./node --nodeConfig="${CONFIG}" --runDir="${RUNDIR}" --eventLog &> "${RUNDIR}/node.log" &
 done
 
-echo ./client --clientConfig bootstrap.d/client0/config/client-config.yamlclient-config.yaml 
+# Run client.
+CL_CONFIG="bootstrap.d/client0/config/client-config.yaml"
+CL_RUNDIR="bootstrap.d/client0/run/"
+mkdir -p "${CL_RUNDIR}"
+sleep 10
+./client --clientConfig "${CL_CONFIG}" &> "${CL_RUNDIR}/client.log"
+sleep 10
+
+# Stop nodes.
+killall node
+wait
