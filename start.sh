3235b79a9805b4f307e70485689e0f045fac3114
Matej Pavlovic <mpa@zurich.ibm.com>
2021-02-08 15:39:50 +0100

    Add optional output dir parameter to start.sh
    Signed-off-by: Matej Pavlovic <mpa@zurich.ibm.com>

--- start.sh
+++ start.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
+# Set output directory
+if [ -n "$1" ]; then
+  OUTDIR="$1"
+else
+  OUTDIR=bootstrap.d
+fi
+
 # Create binaries.
 go build ./cmd/bootstrap
 go build ./cmd/node
@@ -9,22 +16,22 @@ go build ./cmd/client
 
 # Clean up.
 killall node client &> /dev/null || true
-rm -Rf bootstrap.d
-mkdir -p bootstrap.d
+rm -Rf "${OUTDIR}"
+mkdir -p "${OUTDIR}"
 
 # Generate new configuration.
-./bootstrap
+./bootstrap --outputDir="${OUTDIR}"
 
 # Start nodes.
 for ii in $(seq 0 3) ; do
-	CONFIG="bootstrap.d/node${ii}/config/node-config.yaml"
-	RUNDIR="bootstrap.d/node${ii}/run/"
+	CONFIG="${OUTDIR}/node${ii}/config/node-config.yaml"
+	RUNDIR="${OUTDIR}/node${ii}/run/"
 	./node --nodeConfig="${CONFIG}" --runDir="${RUNDIR}" --eventLog &> "${RUNDIR}/node.log" &
 done
 
 # Run client.
-CL_CONFIG="bootstrap.d/client0/config/client-config.yaml"
-CL_RUNDIR="bootstrap.d/client0/run/"
+CL_CONFIG="${OUTDIR}/client0/config/client-config.yaml"
+CL_RUNDIR="${OUTDIR}/client0/run/"
 mkdir -p "${CL_RUNDIR}"
 sleep 10
 ./client --clientConfig "${CL_CONFIG}" &> "${CL_RUNDIR}/client.log"
